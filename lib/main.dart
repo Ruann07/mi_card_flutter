4c338bdce2ab45da59be2ec56beded65aca47260
Ruann Lima <ruannlima07@gmail.com>
2020-04-24 17:14:32 -0300

    mi_card_Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,87 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MaterialApp(debugShowCheckedModeBanner: false, home: Myapp()),
   );
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.white,
+              backgroundImage: AssetImage('images/perfil.png'),
+            ),
+            Text(
+              'Ruann Lima',
+              style: TextStyle(
+                  fontFamily: 'Lobster',
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  fontFamily: 'RobotoCondensed',
+                  color: Colors.teal.shade50,
+                  fontSize: 12.0,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.blue.shade100,
+              ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    '+55 86 12345 6789',
+                    style: TextStyle(
+                        color: Colors.grey.shade900,
+                        fontSize: 15.0,
+                        fontFamily: 'RobotoCondensed'
+                    ),
+                  ),
+                )
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    'Ruann@email.com ',
+                    style: TextStyle(
+                        color: Colors.blueGrey.shade900,
+                        fontSize: 15.0,
+                        fontFamily: 'RobotoCondensed'
+                    ),
+                  ),
+                )
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
